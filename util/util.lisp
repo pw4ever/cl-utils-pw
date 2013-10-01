ad2a1e36df56acccc025b2c992b22d80b600d424
Wei Peng <wpeng1985@gmail.com>
2013-10-01 10:57:12 -0400

    util: make random-* function behave more predictable---:random-state keyword param changes with each, unless bound to a copy before calling

--- util/util.lisp
+++ util/util.lisp
@@ -5,10 +5,10 @@
   `(apply #'(lambda (&key ,@keywords &allow-other-keys) ,@body) ,args))
 
 (defun random-select-from-list (list &key random-state destructive)
-  (let ((random-state (if random-state random-state *random-state*))
+  (let ((*random-state* (if random-state random-state *random-state*))
 	(list (if destructive list (copy-list list))))
     (nbutlast (shuffle list) (random (length list)))))
 
 (defun random-list (limit list-length &key random-state)
-  (let ((random-state (if random-state random-state *random-state*)))
-    (loop repeat list-length collect (random limit random-state))))
+  (let ((*random-state* (if random-state random-state *random-state*)))
+    (loop repeat list-length collect (random limit))))
